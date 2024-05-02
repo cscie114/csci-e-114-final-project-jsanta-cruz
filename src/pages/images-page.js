ef29c0bad0b6c885fd2d8d92d50cc23401f91c49
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-05-01 23:52:18 -0400

    refining slick carousel

--- src/pages/images-page.js
+++ src/pages/images-page.js
@@ -2,74 +2,76 @@ import * as React from "react"
 import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout"
 import Footer from "../components/footer"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const settings = {
-    autoPlay: true,
+    autoPlay: false,
     arrows: true,
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
   };
 
 const ImagesPage = (props) => {
-    const images = props.data.allImages.nodes;
-    // const image = getImage(data.allImages.nodes?.baseimageurl)
+  const images = props.data.allImages.nodes;
+  // const image = getImage(data.allImages.nodes?.baseimageurl)
 
-    return (
-        
-            <div>
-            {images.map((img) => {
-              return (
-                <div key={img.id}>
-                    <div>
-                      <div>{img?.alttext}</div>
-                      <Slider {...settings}>
-                      <img
-                        loading="lazy"
-                        src={img?.baseimageurl}
-                        width={100}
-                        alt={img?.alttext}
-                      />
-                                </Slider>
-
-                    </div>
-                </div>
-              );
-            })}
+  return (
+    <div>
+    <Layout pageTitle="All Images">
+      <Slider {...settings}>
+        {images.map((img) => {
+          return (
+            <div key={img.id}>
+              <img
+                loading="lazy"
+                src={img?.baseimageurl}
+                width={200}
+                alt={img?.alttext}
+              />
             </div>
-      
-    );
-    // return (
-    //   <div>
-    //     <Layout pageTitle="All Images">
-    //       <ul>
-    //         {images.map((img) => {
-    //           return (
-    //             <li key={img.id}>
-    //                 <div>
-    //                   <div>{img?.alttext}</div>
-    //                   <img
-    //                     loading="lazy"
-    //                     src={img?.baseimageurl}
-    //                     width={100}
-    //                     alt={img?.alttext}
-    //                   />
-    //                 </div>
-    //             </li>
-    //           );
-    //         })}
-    //       </ul>
-    //     </Layout>
-    //     <Footer></Footer>
-    //   </div>
-    // );
-    }
+          );
+        })}
+      </Slider>
+    </Layout>
+    </div>
+  );
+}
+    
   export const query = graphql`
   query ImagesPageQuery {
     allImages {
