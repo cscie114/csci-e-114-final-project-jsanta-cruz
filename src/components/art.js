e4fadb8bba285e59b4a346402df019b4d234b185
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-05-05 23:38:39 -0400

    7th attempt

---
+++ src/components/art.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import {objectContain} from "../components/slideshow.module.css"
+
+
+const Art = ({ art }) => {
+    const styles = {
+        galleryImg: {
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          listStyle: "none",    
+        },
+      };
+    return (
+      <div>
+          <ul style={styles.galleryImg}>
+              <li width={200}>
+                <div width={200}>
+                  <img style={styles.imgList}
+                  className={objectContain}
+                    loading="lazy"
+                    src={art?.baseimageurl}
+                    width={200} height={200}
+                    alt={art?.alttext}
+                  />
+                  <p width={200}>{art?.caption}</p>
+                  <p width={200}>{art?.alttext}</p>
+                </div>
+              </li>
+        </ul>
+      </div>
+    );
+  };
+  
+  
+  export default Art;
