9e542a967ad5384d42d07b68b8aab11370fa8aaf
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2026-03-04 11:44:27 -0500

    adding C360 webSDK script to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./footer.module.css"
 
 const Footer = () => {
-  return <footer>Example footer developed for CSCI E-114, Spring 2024.</footer>;
+  return <div><footer>Example footer developed for CSCI E-114, Spring 2024.</footer><div><script src="https://cdn.c360a.salesforce.com/beacon/c360a/14cb6392-68cb-4960-8a09-ca0466611581/scripts/c360a.min.js"></script></div></div>;
 };
 
 export default Footer;
